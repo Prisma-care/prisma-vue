f151070b0d4066a2da4bef2bc7e1e67a1396d4ad
Yens Broothaers <yens.broothaers@student.arteveldehs.be>
2018-07-23 16:06:59 +0200

    Add invite page

--- components/Header.vue
+++ components/Header.vue
@@ -22,6 +22,5 @@
       </div>
 
     </nav>
-
   </header>
 </template>
--- pages/invite/index.vue
+++ pages/invite/index.vue
@@ -1,134 +1,93 @@
 <template>
-  <div class="email-invite">
-    <div class="email-form">
-      <div class="container">
-        <div v-if="step === 1">
-          <div class="row">
-            <div class="col-lg-12 mt-4">
-              <h1>Nodig familie uit om mee verhalen te verzamelen</h1>
-              <p>Stuur hen een uitnodiging via email voor Prisma</p>
-            </div>
-          </div>
-          <div class="row">
-            <b-form action="" class="col-lg-12">
-              <div class="row">
-                <b-form-group label="Voornaam" label-for="firstname" class="col-lg-6 name">
-                  <b-form-input type="text" v-model="form.firstname">
-                  </b-form-input>
-                </b-form-group>
-                <b-form-group label="Familienaam" label-for="lastname" class="col-lg-6 name">
-                  <b-form-input type="text" v-model="form.lastname"></b-form-input>
-                </b-form-group>
-              </div>
-              <b-form-group label="Email" label-for="email">
-                <b-form-input type="email" v-model="form.email">
-                </b-form-input>
-              </b-form-group>
-              <b-form-group label="Onderwerp" label-for="subject">
-                <b-form-input type="text" v-model="form.subject">
-                </b-form-input>
-              </b-form-group>
-              <b-btn @click="next" :disabled="isValid">Volgende stap</b-btn>
-            </b-form>
-          </div>
+  <div id="root" class="container mt-4">
+  
+  <h1 class="mb-2">Vraag om hulp via email</h1>
+  <p class="lead">Wie helpt je met het verzamelen van verhalen?</p>
+  <transition name="fade">
+  <div class="alert alert-success" role="alert" v-if="alert === true">
+    <h4 class="alert-heading">Je email is verzonden!</h4>
+    <p>Ken je nog iemand die...? </p>
+    <hr>
+    <a href="/residents/1/family" class="btn btn-primary">Terug naar overzicht</a>
+  </div>
+  </transition>
+  
+  <form action="">
+    <div class="form-row">
+      <div class="col-2 col-md-3">
+        <div class="form-group firstname">
+          <input type="text" v-model="form.firstname" class="form-control" placeholder="">
+          <label for="firstname">Voornaam</label>
         </div>
-        <div v-if="step ===2">
-          <div class="row">
-            <div class="col-lg-12 mt-4">
-              <h1>Schrijf een gepersonaliseerd bericht</h1>
-            </div>
-          </div>
-          <div class="row">
-            <div class="col-lg-12 mt-4">
-              <h5>Waarvoor heb je hulp nodig?</h5>
-            </div>
-            <div class="col-lg-3">
-              <button class="option" @click="changeExample($event)" value="general">Alles</button>
-              <button class="option" @click="changeExample($event)" value="digital">Foto's digitaliseren</button>
-              <button class="option" @click="changeExample($event)" value="search">Beelden opzoeken</button>
-              <button class="option" @click="changeExample($event)" value="interview">Familie interviewen</button>
-            </div>
-            <div class="col-lg-9">
-              <p>{{example[current_example]}}</p>
-            </div>
-          </div>
-          <div class="row">
-            <div class="col-lg-12 mt-3">
-                  <h5>Stuur een persoonlijk bericht</h5>
-                  <b-form-textarea
-                  v-model="message"
-                  placeholder="Schrijf hier je persoonlijke boodschap"
-                  class="mb-3"
-                  >
-                  </b-form-textarea>
-                <b-btn variant="primary" class="btn-left" @click="prev">Vorige stap</b-btn>
-                <b-btn variant="primary"  class="btn-right" :disabled="isValidMessage">Verzenden</b-btn>
-            </div>
-          </div>
+      </div>
+      <div class="col-2 col-md-3">
+        <div class="form-group lastname">
+          <input type="text" v-model="form.lastname" class="form-control" placeholder="">
+          <label for="lastname">Familienaam</label>
+        </div>
+      </div>
+      <div class="col-md-6">
+        <div class="form-group">
+          <input type="email" v-model="form.email" class="form-control" placeholder="">
+          <label for="email">Email adres</label>
         </div>
       </div>
     </div>
-    <div class="email-expl" v-if="step === 1">
-      <p></p>
+
+    <div class="form-group">
+      <input type="text" class="form-control" placeholder="Onderwerp" value="Verzamelen van herinneringen in foto en tekst">
+      <label for="lastname">Onderwerp van de email</label>
     </div>
-    <div class="email-expl" v-if="step === 2">
-      <div class="email-template-header">
-        <p>Zo zal je email er uit zien</p>
-      </div>
-      <div class="email-info">
-        <p><span>Aan: </span>{{form.email}}</p>
-      </div>
-      <div class="email-info">
-        <p><span>Onderwerp: </span>{{form.subject}}</p>
-      </div>
-      <div class="email-body">
-        <p>{{message}}</p>
-      </div>
+
+  </form>
+
+  <div class="row">
+    <div class="col-md-7">
+      <textarea placeholder="Schrijf hier je persoonlijke boodschap" class="mb-3 form-control" rows="8" v-model="message">@{{message}}</textarea>
+
+      <p>In de email komt automatisch meer info onder je bericht over hoe Prisma werk.
+        <a href="#">Bekijk email voorvertoning</a>
+      </p>
+
+      <button class="btn btn-primary btn-right" @click="send" :disabled="isValidMessage">Verzend email</button>
 
     </div>
+    <div class="col-md-5">
+      <div class="card bg-light border-light">
+        <div class="card-header">
+          <h6 class="mb-0">Voorbeeld tekst</h6>
+        </div>
+
+        <div class="card-body">
+          <div class="example-text" v-html="example">
+            @{{example}}
+          </div>
+          <p class="card-text mt-2">
+            <button class="btn btn-sm btn-light" @click="fillMessage" :disabled="isMessageFilled">Gebruik deze tekst in je email</button>
+          </p>
+        </div>
+      </div>
+    </div>
   </div>
+
+</div>
 </template>
 
 <script>
 export default {
-  middleware: "authenticated",
+  middleware: "authentication",
   data() {
     return {
-      step: 1,
-      example: {
-        general: `Ik wil van oma al haar mooie herinneringen en verhalen vastleggen, zodat we haar leven mooi kunnen documenteren.
-      Om dit te doen is er een applicatie genaamd Prisma.
-      Heb je deze week ergens een halfuurtje om een verhaal met een foto toe te voegen?
-      Deze foto's kunnen van haar zelf zijn, maar je kan ook gerust een foto van het internet gebruiken.
-      Hieronder vind je een link naar een formulier waar je het verhaal kan toevoegen en de andere om het verhaal te bekijken op haar profiel.
-      Groetjes Cedric`,
-        digital: `Ik wil van oma al haar mooie herinneringen en verhalen vastleggen, zodat we haar leven mooi kunnen documenteren.
-      Om dit te doen is er een applicatie genaamd Prisma.
-      Heb je deze week ergens een halfuurtje om een verhaal met een foto toe te voegen?
-      Deze foto's kunnen van haar zelf zijn, maar je kan ook gerust een foto van het internet gebruiken.
-      Hieronder vind je een link naar een formulier waar je het verhaal kan toevoegen en de andere om het verhaal te bekijken op haar profiel.
-      Groetjes Cedric`,
-        interview: `Ik wil van oma al haar mooie herinneringen en verhalen vastleggen, zodat we haar leven mooi kunnen documenteren.
-      Om dit te doen is er een applicatie genaamd Prisma.
-      Heb je deze week ergens een halfuurtje om een verhaal met een foto toe te voegen?
-      Deze foto's kunnen van haar zelf zijn, maar je kan ook gerust een foto van het internet gebruiken.
-      Hieronder vind je een link naar een formulier waar je het verhaal kan toevoegen en de andere om het verhaal te bekijken op haar profiel.
-      Groetjes Cedric`,
-        search: `Ik wil van oma al haar mooie herinneringen en verhalen vastleggen, zodat we haar leven mooi kunnen documenteren.
-      Om dit te doen is er een applicatie genaamd Prisma.
-      Heb je deze week ergens een halfuurtje om een verhaal met een foto toe te voegen?
-      Deze foto's kunnen van haar zelf zijn, maar je kan ook gerust een foto van het internet gebruiken.
-      Hieronder vind je een link naar een formulier waar je het verhaal kan toevoegen en de andere om het verhaal te bekijken op haar profiel.
-      Groetjes Cedric`
-      },
-      current_example: "general",
       form: {
         firstname: "",
         lastname: "",
         email: "",
         subject: ""
       },
-      message: ""
+      message: "",
+      alert: false,
+      example: `<p class="card-text">Met de familie verzamelen we verhalen van Lea die ik aan het rusthuis ga geven. Hiermee kan het personeel Lea dan beter leren kennen. </p>
+    <p class="card-text">Help je mee? Kan je deze week de tijd nemen om enkele belangrijke foto's te scannen?</p>`
     };
   },
   computed: {
@@ -140,6 +99,9 @@ export default {
         ? false
         : "disabled";
     },
+    isMessageFilled() {
+      return this.message.length <= 5 ? false : "disabled";
+    },
     isValidMessage() {
       return this.message !== "" && this.message.length >= 5
         ? false
@@ -147,15 +109,19 @@ export default {
     }
   },
   methods: {
-    prev() {
-      this.step--;
+    fillMessage() {
+      const htmlMessage = this.example;
+      const plainMessage = htmlMessage.replace(/<[^>]+>/g, "");
+      this.message = plainMessage;
     },
-    next() {
-      this.step++;
-    },
-    changeExample(e) {
-      this.current_example = e.target.value;
-      e.target.value.addClass;
+    send() {
+      this.form.lastname = "";
+      this.form.firstname = "";
+      this.form.email = "";
+      this.alert = true;
+      setTimeout(() => {
+        this.alert = false;
+      }, 5000);
     }
   }
 };
