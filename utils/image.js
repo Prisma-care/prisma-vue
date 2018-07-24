96f36b9a852cb3aa29a2f86098d32f95f99de2ed
Terencio Agozzino <terencio.agozzino@gmail.com>
2018-07-24 09:34:20 +0200

    Better image utils

--- utils/image.js
+++ utils/image.js
@@ -1,4 +1,13 @@
+/**
+ * Converts an array buffer to data URL.
+ *
+ * @param {array} buffer
+ * @param {string} type
+ * @returns {string}
+ */
 export default (buffer, type) => {
-  const base64 = btoa([].reduce.call(new Uint8Array(buffer), (p, c) => p + String.fromCharCode(c), ''));
+  const base64 = btoa(
+    [].reduce.call(new Uint8Array(buffer), (p, c) => p + String.fromCharCode(c), '')
+  );
   return `data:${type};base64,${base64}`;
 };
