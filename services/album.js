821b18d330d7d584ce23929bd76f5e2adc683cf0
Terencio Agozzino <terencio.agozzino@gmail.com>
2018-07-25 12:57:57 +0200

    Add documentation for album service

--- services/album.js
+++ services/album.js
@@ -1,18 +1,24 @@
 import axios from 'axios';
+import { baseURL } from '~/config';
 
-import {
-  baseURL
-} from '~/config';
-
-
+/**
+ * Main URL for API access.
+ */
 const URL = `${baseURL}/album`;
 
+/**
+ * Service for interaction with albums.
+ */
 const albumService = {
+
+  /**
+   * Get albums from the list.
+   *
+   * @returns {object}
+   */
   getAlbums() {
-    return axios.get(URL)
-  }
-
-}
-
+    return axios.get(URL);
+  },
+};
 
 export default albumService;
